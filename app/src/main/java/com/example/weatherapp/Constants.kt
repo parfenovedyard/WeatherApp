cef25be9e235196b84c6f445e253bfac6cefc3d2
parfenovedyard <parfenovedyard@gmail.com>
2021-06-30 19:39:54 +0300

    create API call, and get a response

--- app/src/main/java/com/example/weatherapp/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -7,6 +7,10 @@ import android.os.Build
 
 object Constants {
 
+    const val APP_ID: String = ""
+    const val BASE_URL: String = "http://api.openweathermap.org/data/"
+    const val METRIC_UNIT: String = "metric"
+
     fun isNetworkAvailable(context :Context) : Boolean{
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -15,12 +15,19 @@ import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.example.weatherapp.models.WeatherResponse
+import com.example.weatherapp.network.WeatherService
 import com.google.android.gms.location.*
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -122,17 +129,46 @@ class MainActivity : AppCompatActivity() {
             val longitude = mLastLocation.longitude
             Log.i("Current Longitude", "$longitude")
 
-            getLocationWeatherDetails()
+            getLocationWeatherDetails(latitude,longitude )
         }
     }
 
-    private fun getLocationWeatherDetails() {
+    private fun getLocationWeatherDetails(latitude: Double,longitude: Double) {
         if (Constants.isNetworkAvailable(this)) {
-            Toast.makeText(
-                this@MainActivity,
-                "You have connected to Internet.",
-                Toast.LENGTH_SHORT
-            ).show()
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val service : WeatherService = retrofit
+                .create(WeatherService::class.java)
+
+            val listCall: Call<WeatherResponse> = service.getWeather(
+                latitude, longitude, Constants.METRIC_UNIT, getString(R.string.API_key)
+            )
+
+            listCall.enqueue(object : Callback<WeatherResponse>{
+                override fun onResponse(
+                    call: Call<WeatherResponse>,
+                    response: Response<WeatherResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        val weatherList: WeatherResponse? = response.body()
+                        Log.i("Response Result", "$weatherList")
+                    } else{
+                        when (response.code()) {
+                            400 -> Log.i("Error 400", "Bad connection")
+                            404 -> Log.i("Error 404", "Not found")
+                            else -> Log.i("Error", "Generic Error")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                    Log.i("ERRRROOR", t.message.toString())
+                }
+
+            })
         } else{
             Toast.makeText(
                 this@MainActivity,
